29a4ccb27a1509df7747c2281f2b6c658c8febac
Eduardo Alencar <ealencar10@gmail.com>
2015-09-02 13:04:03 -0300

    Imagens sendo exibidas como miniatura ao inves do nome do form

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -1,13 +1,14 @@
 <?php
-$template = '<p><a class="nf_gallery" href="#FILE_URL#" title="#FORM#">#FORM#</a></p>';
+$template = '<p><a class="nf_gallery" href="#FILE_URL#">#FORM#</a></p>';
 $html = "<h1>Galeria de Fotos</h1>";
 
 if(is_array($uploads)) {
 	foreach($uploads as $upload) {
 		$upload_metadata = unserialize($upload->data);
+		$file_url = $upload_metadata['file_url'];
 
-		$photo    = str_replace("#FILE_URL#", $upload_metadata['file_url'], $template);
-		$photo    = str_replace("#FORM#", $upload->meta_value, $photo);
+		$photo    = str_replace("#FILE_URL#", $file_url, $template);
+		$photo    = str_replace("#FORM#", '<img src="'.$file_url.'" width="50" height="50" >', $photo);
 		$html .= $photo;
 	}
 	
