2b29202c257186f0f0c3b10526f43af685da4cf0
Eduardo Alencar <ealencar10@gmail.com>
2015-09-02 13:07:13 -0300

    Titulo da imagem como nome do form

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -1,5 +1,5 @@
 <?php
-$template = '<p><a class="nf_gallery" href="#FILE_URL#">#FORM#</a></p>';
+$template = '<p><a class="nf_gallery" href="#FILE_URL#" title="#FORM_ID#">#FORM#</a></p>';
 $html = "<h1>Galeria de Fotos</h1>";
 
 if(is_array($uploads)) {
@@ -9,6 +9,7 @@ if(is_array($uploads)) {
 
 		$photo    = str_replace("#FILE_URL#", $file_url, $template);
 		$photo    = str_replace("#FORM#", '<img src="'.$file_url.'" width="50" height="50" >', $photo);
+		$photo    = str_replace("#FORM_ID#", $upload->meta_value, $photo);
 		$html .= $photo;
 	}
 	
